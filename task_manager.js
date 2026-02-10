28552b24c0e1817e32b1a48881810a07e416e581
unknown-xenon-star <chiraggujar21@gmail.com>
2026-02-10 18:32:17 +0530

    Updated remove logic

--- task_manager.js
+++ task_manager.js
@@ -4,8 +4,13 @@ const button = document.getElementById("Task_add_button");
 const list = document.getElementById("task_list_ul");
 
 list.addEventListener("click", (event) => {
-    if (event.target.tagName === "LI") {
+    if (event.target.tagName === "SPAN") {
         event.target.classList.toggle("done");
+        saveTasks();
+    }
+    if (event.target.classList.contains("del-btn")) {
+        event.target.parentElement.remove();
+        saveTasks();
     }
 });
 
@@ -16,11 +21,6 @@ function ensureDeleteButton(li) {
     const delBtn = document.createElement("button");
     delBtn.textContent = "❌";
     delBtn.className = "del-btn";
-
-    delBtn.addEventListener("click", (e) => {
-        e.stopPropagation();
-        li.remove();
-    })
     
     li.appendChild(delBtn);
 
@@ -32,13 +32,25 @@ document.querySelectorAll("#task_list_ul li").forEach(ensureDeleteButton);
 button.addEventListener("click", () => {
     if (input.value === "") return ;
 
-    const li = document.createElement("li");
-    li.textContent = input.value;
-
+    const li = document.createElement("li");   
     const span = document.createElement("span");
+
+    span.textContent = input.value;
     
+    li.appendChild(span);
     ensureDeleteButton(li);
-    
+
     list.appendChild(li);
     input.value = "";
+    saveTasks();
 })
+
+function saveTasks() {
+    localStorage.setItem("tasks", list.innerHTML);
+}
+
+function loadTasks() {
+    list.innerHTML = localStorage.getItem("tasks") || "";
+}
+
+loadTasks();
